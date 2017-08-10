dc2e0a9392552c5ce98ca4514d2bf9d21915eb29
puchunjie <puchunjie@live.com>
2017-08-10 10:34:09 +0800

    质检页面预留页

--- src/pages/qualityTesting/index.vue
+++ src/pages/qualityTesting/index.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="co">
-        <span class="iconfont icon-fixing"></span>
+        <com-header>质检</com-header>
+        <span class="iconfont icon-fixing sg"></span>
+        <p>攻城狮正在施工中！</p>
     </div>
 </template>
 
 <script>
-export default {
-  
-}
+    import comHeader from '@/components/business/commonHead'
+    export default {
+        components: {
+            comHeader
+        }
+    }
 </script>
 
 
@@ -16,5 +21,11 @@ export default {
         width: 100%;
         height: 100%;
         background-color: #fff;
+        text-align: center;
+    }
+    .sg{
+        display: inline-block;
+        margin-top: .3rem;
+        font-size: .6rem;
     }
 </style>
