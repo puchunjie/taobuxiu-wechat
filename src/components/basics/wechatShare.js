0545d229de6d8b6bfe77b5db3cdddff0366f9b7c
puchunjie <puchunjie@live.com>
2017-08-30 14:52:18 +0800

    微信分享话术修改

--- src/components/basics/wechatShare.js
+++ src/components/basics/wechatShare.js
@@ -31,9 +31,9 @@ export default {
                     this.$wechat.config(this.wxconfig);
                     this.$wechat.ready(() => {
                         this.$wechat.onMenuShareTimeline({
-                            imgUrl: "http://www.itaobuxiu.com/files/2017/5/15/4NPzgZ5bASPn.png",
+                            imgUrl: "http://www.itaobuxiu.com/files/ads/ads_mobile/taobuxiu_logo.png",
                             link: window.location.href,
-                            title: "淘不锈 | 全国首家提供上门质检的不锈钢全产业链绿色服务平台",
+                            title: "淘不锈 | 不锈钢全产业链绿色服务平台",
                             type: "link",
                             success: function() {
                                 // 用户确认分享后执行的回调函数
@@ -43,10 +43,10 @@ export default {
                             }
                         });
                         this.$wechat.onMenuShareAppMessage({
-                            desc: "淘不锈”专注于最低价、最靠谱、最新鲜、最全面的不锈钢特价资源。价格打折、品质不打折，专业的质检服务为您保驾护航！",
-                            imgUrl: "http://www.itaobuxiu.com/files/2017/5/15/4NPzgZ5bASPn.png",
+                            desc: "全国首家提供上门质检的不锈钢集采、加工、物流、金融一站式绿色服务平台！",
+                            imgUrl: "http://www.itaobuxiu.com/files/ads/ads_mobile/taobuxiu_logo.png",
                             link: window.location.href,
-                            title: "淘不锈 | 全国首家提供上门质检的不锈钢全产业链绿色服务平台",
+                            title: "淘不锈 | 不锈钢全产业链绿色服务平台",
                             success: function() {
                                 // 用户确认分享后执行的回调函数
                             },
